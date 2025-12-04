3f9e62046317e2947f34d57e2ed18385d26c2128
Arisa-Laloo <alaloo2022@my.fit.edu>
2025-12-04 15:14:05 -0500

    finished simplified version- added shuffle and loop and redid the board output

--- contest2/Contest2.asm
+++ contest2/Contest2.asm
@@ -1,9 +1,4 @@
-; ------------------------------------------------------
-; Simple Memory Match Skeleton
-; Prints board, gets 2 picks, checks match
-; MASM + Irvine32
-; ------------------------------------------------------
-
+;Memory Matching game
 INCLUDE Irvine32.inc
 
 .data
@@ -11,35 +6,109 @@ INCLUDE Irvine32.inc
     ; Positions: 1 2 3 4
     cards BYTE  'A','A','B','B', 'C', 'C', 'D', 'D'
 
-    board   BYTE "Board Positions: 1 2 3 4 5 6 7 8",0
+    dispboard   BYTE '1','2','3','4','5','6','7','8'
+    promptBoard BYTE "Board: ", 0
     promptStart	BYTE "Pick first card position(1-8): ", 0
     promptNextPick BYTE "Enter second card position (1-8): ",0
+
+    showGuess1 Byte "Value at first pick: ", 0
+    showGuess2 Byte "Value at second pick: ", 0
+
     matchMsg   BYTE "Match!",0
     noMatchMsg BYTE "Not a match!",0
+    promptExit BYTE "Exiting game.",0
+    newRound BYTE "New Round"
+    pairs DWORD 0
+    promptWin BYTE "All Pairs Found. You Win!!", 0
 
 .code
+
+Shuffle PROC
+    mov ecx, 7          ; start from last index (7)
+
+ShuffleLoop:
+    ; j = RandomRange(i+1)
+    mov eax, ecx
+    inc eax
+    call RandomRange    ; 0 to eax-1
+    mov ebx, eax        ; EBX = j index
+
+    ; swapping cards[ecx] and cards[ebx] for shuffling
+    mov al, cards[ecx]
+    mov dl, cards[ebx]
+
+    mov cards[ecx], dl
+    mov cards[ebx], al
+
+    loop ShuffleLoop
+    ret
+Shuffle ENDP
+
+
 main PROC
+call Randomize
+call Shuffle
+GameLoop:
+    ;New round
+    mov edx, OFFSET newRound
+    call WriteString
+    call Crlf
 
     ;print board
-    mov edx, OFFSET board
+    mov edx, OFFSET promptBoard
     call WriteString
-    call CrLf
     
-    ;prompt for pick 1
+    mov esi, OFFSET dispboard
+    mov ecx, 8
+
+printBoard:
+    mov al, [esi]
+    movzx eax, al
+    call WriteChar
+
+    mov al, ' '
+    call WriteChar
+
+    inc esi
+    loop PrintBoard 
+
+    call Crlf
+    
+    ;prompt 1st pick
     mov edx, OFFSET promptStart
     call WriteString
     call ReadInt
-    mov ebx, eax      ; store pick1 (1-4)
-    dec ebx           ; convert to array index (0-3)
 
-    ; prompt for pick 1
+    cmp eax, 0        ; check if user entered 0
+    je EndGame        ;exit if 0
+
+    mov ebx, eax      ; store 1st pick(1-8)
+    dec ebx           ; 0 - 7
+    
+    mov edx, OFFSET showGuess1
+    call WriteString
+
+    mov al, cards[ebx]
+    movzx eax, al
+    call WriteChar
+    call Crlf
+
+    ; prompt for pick 2
     mov edx, OFFSET promptNextPick
     call WriteString
     call ReadInt
-    mov ecx, eax      ; store pick2
+    mov ecx, eax      ; store 2nd pick
     dec ecx           ; convert to index
 
-    ;get card values
+    mov edx, OFFSET showGuess2
+    call WriteString
+
+    mov al, cards[ecx]
+    movzx eax, al
+    call WriteChar
+    call Crlf
+
+    ;save card values
     mov al, cards[ebx]   ; value at pick1
     mov dl, cards[ecx]   ; value at pick2
 
@@ -50,14 +119,31 @@ main PROC
 match:
     mov edx, OFFSET matchMsg
     call WriteString
-    jmp done
+    call Crlf
+    inc pairs
+    jmp ContinueLoop
 
 notMatch:
     mov edx, OFFSET noMatchMsg
     call WriteString
 
-done:
-    call CrLf
+ContinueLoop:
+    
+    cmp pairs, 4
+    je AllFound
+    call Crlf
+    jmp GameLoop        ;next round
+
+AllFound:
+    mov edx, OFFSET promptWin
+    call WriteString
+    call Crlf
+    exit
+
+EndGame:
+    mov edx, OFFSET promptExit
+    call WriteString
+    call Crlf
     exit
 
 main ENDP
